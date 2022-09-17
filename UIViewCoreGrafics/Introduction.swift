627eee53f7634e5254d610ad0bf7efc5fa401269
komplektan <komplektan@mail.ru>
2022-09-17 23:27:34 +0500

    Setup project

---
+++ UIViewCoreGrafics/Introduction.swift
@@ -0,0 +1,15 @@
+//
+//  Introduction.swift
+//  UIViewCoreGrafics
+//
+//  Created by Вячеслав Квашнин on 17.09.2022.
+//
+
+import UIKit
+
+class Introduction: UIView {
+
+    override func draw(_ rect: CGRect) {
+         
+    }
+}
